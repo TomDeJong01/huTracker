429883752745f5e89f17b50b80b476b80b03ece7
TomDeJong01 <dejongtom01@gmail.com>
2020-05-26 21:35:44 +0200

    initial commit

---
+++ scripts/rectangle.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    id = 0
+    x = 0
+    y = 0
+    w = 0
+    h = 0
+    firstFrame = 0
+    lastFrame = 0
+
+    def __intit__(self, i, x, y, w, h, ff, lf):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.firstFrame = ff
+        self.lastFrame - lf
