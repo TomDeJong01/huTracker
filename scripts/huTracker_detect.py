9d73a6da3a963299d69ff6b47086e24422aa8836
Tom <dejongtom01@gmail.com>
2020-06-09 15:33:42 +0200

    vzoveel

--- scripts/huTracker_detect.py
+++ scripts/huTracker_detect.py
@@ -7,13 +7,15 @@ import json
 import time
 import cv2
 
-#json params
+
+# json params
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--conf", required=True, help="conf.json")
 args = vars(ap.parse_args())
 conf = json.load(open(args["conf"]))
 
-#init camera
+
+# init camera
 camera = PiCamera()
 camera.resolution = tuple(conf["resolution"])
 camera.framerate = conf["fps"]
@@ -23,10 +25,12 @@ print("[INFO] warming up...")
 time.sleep(conf["camera_warmup_time"])
 avg = None
 
-#init application vars
+
+# init application vars
 motionCounter = 0
 frameNr = 0
 recObject = {"id": 0, "x": 0, "y": 0, "w": 0, "h": 0, "lf": 0, "tf": 0}
+lastFrame = 0
 yList = []
 
 
@@ -50,22 +54,21 @@ def count(action):
 
 
 def countPersen(yList):
-    print("NEW LIST PRINT")
+    print(" ")
     for i in range(len(yList)):
-        if yList[i] == yList[i +1]:
-            continue
-        if yList[i] < yList[i + 1]:
-            count("remove")
-            conf["amountOfPresentPeople"] = getVars()["amountOfPresentPeople"]
-            print("new count: " + str(conf["amountOfPresentPeople"] ))
-            yList = []
-            break
-        else:
-            count("add")
-            newTotal = getVars()["amountOfPresentPeople"]
-            print("new count: " + str(newTotal))
-            yList = []
-            break
+        if i + 1 != len(yList):
+            if yList[i] == yList[i + 1]:
+                continue
+            if yList[i] < yList[i + 1]:
+                count("remove")
+                conf["amountOfPresentPeople"] = getVars()["amountOfPresentPeople"]
+                print("new count: " + str(conf["amountOfPresentPeople"] ))
+                break
+            else:
+                count("add")
+                conf["amountOfPresentPeople"] = getVars()["amountOfPresentPeople"]
+                print("new count: " + str(conf["amountOfPresentPeople"]))
+                break
 
 
 for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -80,6 +83,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
         avg = gray.copy().astype("float")
         rawCapture.truncate(0)
         continue
+
     cv2.accumulateWeighted(gray, avg, 0.5)
     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
@@ -88,29 +92,22 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
-    if frameNr > recObject['lf'] + 2:
-        # nieuw object
-        if recObject['id'] > 9999:
-            recObject['id'] = 1
-        if len(yList) > 1:
-            countPersen(yList)
-            yList=[]
+    if frameNr > lastFrame + 2 and len(yList) > 1:
+        countPersen(yList)
+        yList = []
 
     for c in cnts:
         if cv2.contourArea(c) < conf["min_area"]:
             continue
         (x, y, w, h) = cv2.boundingRect(c)
+        lastFrame = frameNr
 
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         if y == 0:
-            continue
-
-            recObject = {"id": recObject['id'] + 1, "x": x, "y": y, "w": w, "h": h, "lf": frameNr, "tf": 0}
             yList = [y]
+            continue
         else:
-            recObject = {"id": recObject['id'], "x": x, "y": y, "w": w, "h": h, "lf": frameNr, "tf": recObject["tf"] + 1}
             yList.append(y)
-            #update object
 
     if conf["show_video"]:
         cv2.imshow("Security Feed", frame)
