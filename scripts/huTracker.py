2951cfbc01d2b6f0f734398ee3c1feb5017fff97
Tom <dejongtom01@gmail.com>
2020-07-06 16:24:33 +0200

    auto register

--- scripts/huTracker.py
+++ scripts/huTracker.py
@@ -1,8 +1,6 @@
 from datetime import datetime
-
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-import numpy as np
 import requests
 import argparse
 import imutils
@@ -17,15 +15,17 @@ args = vars(ap.parse_args())
 local_json_file = open(args["conf"], "r")
 conf = json.load(local_json_file)
 conf["count"] = 0
+conf["id"] = False
 local_json_file.close()
 
 # init camera
 camera = PiCamera()
 camera.resolution = tuple(conf["resolution"])
 camera.framerate = conf["fps"]
-camera.rotation = conf["rotation"]
+camera.rotation = conf["rotation"]  # 0 = strip ->camera = add && camera->strip = remove
 rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 time.sleep(conf["camera_warmup_time"])
+
 
 def update_json():
     json_write = open(args["conf"], "w")
@@ -47,10 +47,10 @@ def register(name, max_people):
     update_json()
 
 
-def set_local_conf(json):
-    conf["title"] = json["title"]
-    conf["maxPeopleCount"] = json["maxPeopleCount"]
-    conf["AmountOfPresentPeople"] = json["amountOfPresentPeople"]
+def set_local_conf(response):
+    conf["title"] = response["title"]
+    conf["maxPeopleCount"] = response["maxPeopleCount"]
+    conf["AmountOfPresentPeople"] = response["amountOfPresentPeople"]
     update_json()
 
 
@@ -62,21 +62,15 @@ def sync_get():
         return False
 
 
-def count_persen(y_list, with_fps=False, start_time=0, end_time=0):
-    print(" ")
-    print("meeting nr: " + str(conf["count"]))
-    conf["count"] +=1
+def count_persen(y_list, test_data=False, start_time=0, end_time=0):
+    conf["count"] += 1
+    second_half = y_list[len(y_list) // 2:]
     if len(y_list) % 2 != 0:
-        second_half = y_list[len(y_list) // 2:]
         first_half = y_list[0:int(len(y_list)+1) // 2]
     else:
         first_half = y_list[0:len(y_list) // 2]
-        second_half = y_list[len(y_list)//2:]
-
     first_average = sum(first_half) / len(first_half)
     second_average = sum(second_half) / len(second_half)
-    print(y_list)
-    print("first_avg:"+str(round(first_average))+" second_avg:"+str(round(second_average)))
 
     if first_average > second_average:
         requests.post(conf["apiUrl"] + str(conf["id"]) + "/add")
@@ -87,16 +81,17 @@ def count_persen(y_list, with_fps=False, start_time=0, end_time=0):
         conf["AmountOfPresentPeople"] -= 1
         print("remove - new count: " + str(conf["AmountOfPresentPeople"]))
 
-    if with_fps:
+    if test_data:
+        print("\nsampling nr: " + str(conf["count"]) + "\n")
+        print(y_list)
+        print("first_avg:" + str(round(first_average)) + " second_avg:" + str(round(second_average)))
         time_ms = (end_time - start_time) / 1000000
-        frames = len(y_list)
-        fps = frames / (time_ms/1000)
-        print("\nframes: " + str(frames) + "\ttime: " + str(round(time_ms)) + " ms\tfps: " + str(fps))
+        fps = len(y_list) / (time_ms/1000)
+        print("frames: " + str(len(y_list)) + "\ttime: " + str(round(time_ms)) + " ms\tfps: " + str(fps) + "\n")
     update_json()
 
 
 def main_loop():
-
     frame_nr = 0
     last_frame = 0
     y_list = []
@@ -120,19 +115,18 @@ def main_loop():
         frame_delta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
         thresh = cv2.threshold(frame_delta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
-        thresh = cv2.dilate(thresh, None, iterations=2)
+        # thresh = cv2.dilate(thresh, None, iterations=2)
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
 
         if frame_nr > last_frame + conf['buffer_frames'] and len(y_list) > 3:
             if conf["get_test_data"]:
                 count_persen(y_list, True, start_time, end_time)
-            else :
+            else:
                 count_persen(y_list)
             y_list = []
 
         for c in cnts:
-
             if cv2.contourArea(c) < conf["min_area"]:
                 continue
             (x, y, w, h) = cv2.boundingRect(c)
@@ -145,19 +139,18 @@ def main_loop():
                     end_time = time.time_ns()
             if conf["capture_img"]:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                # outfile = 'img/img_%s.jpg' % (str(datetime.now()))
-                # cv2.imwrite(outfile, frame)
-
-                rect = cv2.minAreaRect(c)
-                box = cv2.boxPoints(rect)
-                box = np.int0(box)
-                cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
-                outfile = 'img/img_%s.jpg' % (str(datetime.now()))
-                cv2.imwrite(outfile, frame)
+                out_default = '/home/pi/Documents/scripts/img/img_%s.jpg' % (str(datetime.now()))
+                out_thresh = '/home/pi/Documents/scripts/img/thr_img_%s.jpg' % (str(datetime.now()))
+                cv2.imwrite(out_thresh, thresh)
+                cv2.imwrite(out_default, frame)
 
         if conf["show_video"]:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.imshow("Feed", frame)
+            if cnts:
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.imshow("default view", frame)
+            if thresh.any():
+                cv2.imshow("threshold view", thresh)
             key = cv2.waitKey(1) & 0xFF
             if key == ord("q"):
                 break
@@ -166,17 +159,17 @@ def main_loop():
 
 if __name__ == "__main__":
     if conf["id"]:
-        json_vars = sync_get()
-        if json_vars:
-            set_local_conf(json_vars)
+        response = sync_get()
+        if response:
+            set_local_conf(response)
             main_loop()
         else:
-            print("kan geen contact maken met de database")
+            print("kan geen contact maken met de database, probeer het later nogeens")
     else:
-        print("register device")
-        title = input("enter huTracker device name:")
-        maxPeople = "NaN"
-        while not maxPeople.isnumeric() or int(maxPeople) < 0:
-            maxPeople = input("max number of people in room:")
-        register(title, maxPeople)
+        # print("register device")
+        # title = input("enter huTracker device name:")
+        # maxPeople = "NaN"
+        # while not maxPeople.isnumeric() or int(maxPeople) < 0:
+        #     maxPeople = input("max number of people in room:")
+        register("Raspberry Pi", 10)
         main_loop()
